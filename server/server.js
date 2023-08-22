5cab25ae3e0ebfa97ff3d4821c735007028e6e11
Shivam Kumar Singh <shivam0sk98@gmail.com>
2023-08-22 19:39:40 +0530

    retrying non working changes

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ dotenv.config({path:__dirname+'/.env'});
 
 const URI = process.env.MONGODB_URL;
 
-mongoose.connect(URI, (err) => {
+mongoose.connect("mongodb+srv://shivam:Te03GXS4PvfEio1X@cluster0.xz4a32v.mongodb.net/merndb?retryWrites=true&w=majority", (err) => {
   if (err) console.log(err);
   else console.log("MongoDB: Connection Successful");
 });
